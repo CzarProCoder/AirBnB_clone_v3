aa15d1df240fa234b64b7bf8dcd6b8dcd57dfb40
CzarProCoder <juliusczar36@gmail.com>
2023-07-29 13:27:33 +0300

    Create an instance of flask api

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+'''
+Module to initialize an flask instance app
+'''
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown():
+    '''
+    Closes the storage
+    Application context is being torn down
+    '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
